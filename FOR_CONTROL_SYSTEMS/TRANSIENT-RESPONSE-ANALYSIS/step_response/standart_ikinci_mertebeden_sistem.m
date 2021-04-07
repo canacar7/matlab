e40d479dd75093a3c64992a44393ba007f3ea53d
Can Ahmet ACAR <64232218+canacar7@users.noreply.github.com>
2021-04-07 14:41:21 +0300

    Delete FOR_CONTROL_SYSTEMS/TRANSIENT-RESPONSE-ANALYSIS/step_response directory

--- FOR_CONTROL_SYSTEMS/TRANSIENT-RESPONSE-ANALYSIS/step_response/standart_ikinci_mertebeden_sistem.m
+++
@@ -1,5 +0,0 @@
-wn = 5;
-damping_ratio = 0.4;
-[num0,den] = ord2(wn,damping_ratio);
-num = 5^2*num0;
-printsys(num,den,'s')
--- FOR_CONTROL_SYSTEMS/TRANSIENT-RESPONSE-ANALYSIS/step_response/step_response_standart_ikinci_mertebeden_sistem.m
+++
@@ -1,8 +0,0 @@
-num = [25];
-den = [1 4 25];
-t = 0:0.1:4;
-[y,x,t] = step(num,den,t);
-plot(t,y)
-grid
-xlabel('t')
-ylabel('output')
